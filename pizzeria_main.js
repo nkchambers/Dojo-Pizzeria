2559cc9c9000fb5e9de27b75b72f53d738f13621
nkchambers <nkchames@gmail.com>
2023-05-22 23:30:42 -0700

    adding Dojo Pizzeria for 5-22-23

--- pizzeria_main.js
+++ pizzeria_main.js
@@ -8,14 +8,14 @@ function pizzaOven(crustType, sauceType, cheeses, toppings) {
     return pizza;
 }
 
-var p1 = pizzaOven("deep dish", "traditional", ["mozzarella"], ["pepperoni", "sausage"]);
+var p1 = pizzaOven("Deep Dish", "traditional", ["mozzarella"], ["pepperoni", "sausage"]);
 console.log(p1);
 
-var p2 = pizzaOven ("hand tossed", "marinara", ["mozzarella", "feta"], ["mushrooms", "olives", "onions"]);
+var p2 = pizzaOven ("Hand Tossed", "marinara", ["mozzarella", "feta"], ["mushrooms", "olives", "onions"]);
 console.log(p2);
 
-var p3 = pizzaOven("hand tossed", "marinara", ["mozzarella"], ["pepperoni", "sausage", "bacon", "salami"]);
+var p3 = pizzaOven("Hand Tossed", "marinara", ["mozzarella"], ["pepperoni", "sausage", "bacon", "salami"]);
 console.log(p3);
 
-var p4 = pizzaOven("thin", "bbq", ["mozzarella"], ["chicken", "onions"]);
+var p4 = pizzaOven("Thin Crust", "bbq", ["mozzarella"], ["chicken", "onions"]);
 console.log(p4);
